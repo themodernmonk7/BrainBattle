b8700bc9f9868da7754874d82e916187d6bb7e1b
themodernmonk7 <sauravavishek@gmail.com>
2022-10-03 09:01:18 +0530

    Fix small bug

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,5 +1,6 @@
-import React, { useContext } from "react"
-AppContext = React.createContext()
+import React, { useContext, useState } from "react"
+
+const AppContext = React.createContext()
 
 const API_ENDPOINT = `https://opentdb.com/api.php?`
 
@@ -9,7 +10,21 @@ const tempUrl =
   "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple"
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  return (
+    <AppContext.Provider
+      value={{ waiting, loading, questions, index, correct, error }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 
 // custom hook
@@ -17,4 +32,4 @@ export const useGlobalContext = () => {
   return useContext(AppContext)
 }
 
-export { AppProvider, AppContext }
+export { AppContext, AppProvider }
